701a41f52f64ad655237eae26c14df865a7ff8f0
weillercarvalho <engweiller@hotmail.com>
2022-11-09 00:12:30 -0300

    feat [] First EndPoint finished.

---
+++ src/protocols/status.ts
@@ -0,0 +1,11 @@
+export type statusCode = {
+    OK: number,
+    CREATED: number,
+    NO_CONTENT: number,
+    BAD_REQUEST: number,
+    UNAUTHORIZED: number,
+    NOT_FOUND: number,
+    CONFLICT: number,
+    UNPROCESSABLE: number,
+    SERVER_ERROR: number,
+}
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { Request, Response } from 'express';
-import Joi from 'joi';
+import Joi, { boolean } from 'joi';
 import dotenv from 'dotenv';
-
-import {SignUpProtocol} from './protocols/signUp'
-import {SignInProtocol} from './protocols/signIn'
+import {connection} from './database/db.js'
+import { STATUS_CODE } from './enums/statusCode.js';
+import {SignUpProtocol} from './protocols/signUp.js'
+import {SignInProtocol} from './protocols/signIn.js'
+import bcrypt from 'bcrypt';
 
 dotenv.config();
 
@@ -38,7 +40,30 @@ const signupSchema = Joi.object({
 }) 
 
 server.post(`/signup`, async (req: Request,res: Response) => {
-    return res.send(`singup`)
+    const {name, email, password, confirmPassword}: SignUpProtocol = req.body;
+    const validation = signupSchema.validate(req.body, {abortEarly : false})
+    if (validation.error) {
+        const messageError = validation.error.details.map(v => v.message);
+        return res.status(STATUS_CODE.UNPROCESSABLE).send(messageError)
+    }
+    if (!name || !email || !password || !confirmPassword) {
+        return res.sendStatus(STATUS_CODE.UNAUTHORIZED)
+    }
+    if (password !== confirmPassword) {
+        return res.sendStatus(STATUS_CODE.UNAUTHORIZED)
+    }
+    const hashing: string = bcrypt.hashSync(password,10);
+
+    try {
+        const findEmail = await connection.query(`SELECT * FROM users WHERE email = $1`,[email])
+        if (findEmail.rows.length > 0) {
+            return res.sendStatus(STATUS_CODE.CONFLICT);
+        }
+        const query = connection.query(`INSERT INTO users (name,email,password) VALUES ($1,$2,$3)`,[name,email,hashing]);
+        return res.sendStatus(STATUS_CODE.CREATED);
+    } catch (error) {
+        return res.status(STATUS_CODE.SERVER_ERROR).send(error.message)
+    }
 })
 
 server.post(`/signin`, async (req: Request,res: Response) => {
